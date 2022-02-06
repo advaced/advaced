2c059ac26e5ce040c1683998343697b8f4511bfd
diesdasnicolas <diesdasnicolas@gmail.com>
2022-02-06 19:01:31 +0100

    input.py: Add file and handle_input method

---
+++ cmd/input.py
@@ -0,0 +1,11 @@
+def handle_input(arguments: list):
+    '''Handle the input and all its arguments
+
+    :param arguments: The given arguments when the command was executed
+    :type arguments: string
+
+    :raises: :py:class:`cmd.error.InvalidArgument`: If an argument doesn't exist
+    :raises: :py:class:`cmd.error.TooManyArguments`: If too many arguments are used
+    :raises: :py:class:`cmd.error.TooFewArguments`:
+    '''
+    return

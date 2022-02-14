d8dd9a08ba3741cda553db60670102424efc0e2b
diesdasnicolas <diesdasnicolas@gmail.com>
2022-02-14 22:15:47 +0100

    transaction.py: Edit tx-struct, add json-formatter

--- blockchain/transaction.py
+++ blockchain/transaction.py
@@ -1,8 +1,9 @@
 # SHA256 hash-algorithm
 from hashlib import sha256
 
-# Datetime-module
 from datetime import datetime
+import json
+
 
 class Transaction:
     def __init__(self, sender, recipient, amount, fee=None, tx_type='tx', signature=None):
@@ -16,7 +17,7 @@ class Transaction:
         :type amount: int
         :param fee: The transaction-fee, that is added to the incentive for the validators.
         :type fee: int
-        :param type: tx (transaction), stake (staking event), unstake (unstaking event)
+        :param type: tx (transaction), stake (staking event), claim (claiming event), burn (expropriation event)
         :type type: str
         :param signature: Signed hash with the private-key of the sender (or in some cases with the private-key
                           of the recipient).
@@ -50,8 +51,61 @@ class Transaction:
         """Signs the transaction with the private-key of the keypair (in most cases the private-key of the sender)
 
         :param key: The private-key of the sender (in some cases the key of the recipient)
-        :type: str
+        :type key: str
 
         :return: Signature of the transaction-hash and private-key
         """
         return ''
+
+
+    def to_json(self):
+        """For data transportation/storing purposes, a json-format is created
+
+        """
+        tx_dict = {
+            'sender': self.sender,
+            'recipient': self.recipient,
+            'amount': self.amount,
+            'fee': self.fee,
+            'type': self.type,
+            'timestamp': str(self.timestamp),
+            'hash': self.hash,
+            'signature': self.signature
+        }
+
+        return json.dumps(tx_dict)
+
+
+    def from_json(self, json_data) -> bool:
+        """Create transaction from json-data
+
+        :param json_data: String in json-format that includes the transaction-values
+        :type json_data: str
+
+        :return: A status wether the data-load was successful or not
+        :rtype: bool
+        """
+
+        # Convert json to dictionary
+        dict_data = json.loads(json_data)
+
+        try:
+            # Set the data to the transaction
+            self.sender = dict_data['sender']
+            self.recipient = dict_data['recipient']
+            self.amount = dict_data['amount']
+            self.fee = dict_data['fee']
+            self.type = dict_data['type']
+
+            # Check if the signature is included in the json-data
+            if dict_data['signature']:
+                self.signature = dict_data['signature']
+
+            self.timestamp = dict_data['timestamp']
+
+        # Return the status
+        except KeyError:
+
+            return False
+
+        return True

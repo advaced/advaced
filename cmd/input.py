2747e326db8f48c01a490fe9589bc2293e4ef532
diesdasnicolas <diesdasnicolas@gmail.com>
2022-02-19 13:30:18 +0100

    input.py: Prepare for input-handling

--- cmd/input.py
+++ cmd/input.py
@@ -1,4 +1,15 @@
-def handle_input(arguments: list):
+# Add to path
+from sys import path, argv
+import os
+path.insert(0, os.path.join(os.getcwd(), '../'))
+
+# Project version
+from __init__ import __version__
+
+# Valid arguments
+from __init__ import OPTIONS, COMMANDS
+
+def handle_input():
     '''Handle the input and all its arguments
 
     :param arguments: The given arguments when the command was executed

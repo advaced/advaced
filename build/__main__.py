cc8b040e1dc667bd789a66cebd38ba32b65b9077
diesdasnicolas <diesdasnicolas@gmail.com>
2022-02-22 21:13:07 +0100

    __main__.py: Add database init to build

--- build/__main__.py
+++ build/__main__.py
@@ -1,18 +1,29 @@
 # Modules for project-building
 from cmd import build as build_cmd
 from lib import build as build_lib
+from db import build as build_db
 
 
-def build_project() -> int:
+def build_project() -> bool:
     # Build the command(s)
     success_cmd = build_cmd()
 
     # Build the library
     success_lib = build_lib()
 
+    # Check if the builds were successful
     if not success_cmd or not success_lib:
         # Logging for development
         print('build failed')
+
+        return False
+
+    success_db = build_db()
+
+    # Check wether the database-build was successful or not
+    if not success_db:
+        # Logging for development
+        print('database-build failed')
 
         return False
 

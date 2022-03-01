e39ae1ca5f8eba4af62e5b2dcee4ea9a51e289bb
diesdasnicolas <diesdasnicolas@gmail.com>
2022-03-01 16:48:33 +0100

    blockchain.py: Create blockchain to db connection

--- util/database/blockchain.py
+++ util/database/blockchain.py
@@ -1,7 +1,103 @@
 # Add to path
 from sys import path
-import os
-path.insert(0, os.path.join(os.getcwd(), '../../'))
+from os import getcwd
+path.insert(0, getcwd())
 
-# Database handler
-from __init__ import DB
+# Transaction-class
+from blockchain import Transaction
+
+# Database-connector
+from util.database.database import Database
+
+
+def add_block(block_dict: dict, overwrite: bool=False):
+    """Add block to the database.
+
+    :param block_dict: Dictionary of block-information.
+    :type block_dict: dict
+    :param overwrite: If block already exists do or do not overwrite.
+    :type overwrite: bool
+
+    :return: Status if the block was added successful.
+    :rtype: bool
+    """
+
+    # Check if block already exists in the database
+    if fetch_block(block_dict['index']):
+        # Check if overwriting blocks is set to off
+        if not overwrite:
+            return False
+        
+        remove_block(block_dict['index'])
+
+    # Transform transaction-dict to json-format
+    tx = Transaction.from_dict(block_dict['tx'])
+    tx = tx.to_json()
+    block_dict['tx'] = tx
+
+    # Add block to the database
+    success = Database.push_to_db('INSERT INTO blockchain_v1_0_0 VALUES (:index, :previous_hash, :version, :timestamp, \
+                                   :base_fee, :tx, :hash, :validator, :signature)')
+
+    # Check if block was successfully added to the database
+    if not success:
+        return False
+    
+    return True
+
+
+def remove_block(index: int):
+    """Remove block from its index.
+
+    :param index: Index of the block to delete.
+    :type idnex: int
+
+    :return: Status if the block was removed successful.
+    :rtype: bool
+    """
+
+    # Delete block from database
+    success = Database.push_to_db('DELETE FROM blockchain_v1_0_0 WHERE block_index = :id', { 'id': index })
+
+    # Check if execution was not succesful
+    if not success:
+        return False
+    
+    return True
+
+
+def fetch_block(index: int):
+    """Fetch block from its index.
+
+    :param index: Index of the block to fetch.
+    :type idnex: int
+
+    :return: Fetch was not succesful.
+    :rtype: bool
+    :return: Dict-data of the block.
+    :rtype: dict
+    """
+    block_data = Database.fetchone_from_db("SELECT * FROM blockchain_v1_0_0 WHERE block_index = :id", { 'id': index })
+
+    # Check if the response of the database is correct
+    if not block_data:
+        return False
+
+    return block_data
+
+from json import dumps
+print(add_block({
+            'index': 1,
+            'previous_hash': '112',
+
+            'version': 'df',
+            'timestamp': '234',
+
+            'base_fee': 1,
+            'tx': dumps({'sdfsdfsdfsdf': 'dsf'}),            
+
+            'hash': 'sdf',
+            'validator': '345',
+            'signature': '345'
+        }))
+print(fetch_block(1))

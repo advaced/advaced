0bc1053f406c315c128f5def9af6a865479234d0
diesdasnicolas <diesdasnicolas@gmail.com>
2022-03-27 05:30:59 +0200

    processor.py: FInish processor alpha

--- validator/processor.py
+++ validator/processor.py
@@ -2,7 +2,9 @@
 from threading import Thread, Event
 from queue import Queue
 
+# Scheduling
 from time import sleep as time_sleep
+from datetime import datetime, timedelta
 
 # Add to path
 from sys import path
@@ -28,14 +30,18 @@ from accounts import Wallet
 
 
 class Processor():
-    def __init__(self, private_key, start=False):
+    def __init__(self, private_key, start=False, genesis_validation=False):
         self.private_key = private_key
+        self.genesis_validation = genesis_validation
 
         self.stop_event = Event()
         self.thread = Thread(target=self.run)
 
         # Input of the rpc server and the other nodes
         self.input_queue = Queue()
+
+        # Transactions to add to chain
+        self.tx = [ ]
 
         if start:
             self.start()
@@ -50,7 +56,7 @@ class Processor():
 
 
         # 3. Check if enough VAC is staked to become a validator
-        if Wallet.coins(self.validator.wallet.public_key, self.blockchain) < 4_096:
+        if Wallet.stake(self.validator.wallet.public_key, self.blockchain) < 4_096:
             return False
 
         # 4. Advertise to the network
@@ -64,16 +70,76 @@ class Processor():
 
         # 7. Listen to new data and validate blocks
         while not self.stop_event.is_set():
-            pass
-            #
+            print('Creating new block...')
 
-            # TODO -> Add the processor
+            # Fetch the transactions for this block
+            if self.tx:
+                tx = self.tx[0:2048] if len(self.tx) >= 2048 else self.tx[0:len(self.tx)]
+
+                self.tx = list(set(self.tx) - set(tx))
+
+            else:
+                tx = [ ]
+
+            # Create own block
+            block = Block(tx, self.blockchain.last_blocks[0])
+            block.timestamp = self.blockchain.last_blocks[0].timestamp + timedelta(0, 32)
+
+            # Sign the block
+            block, success = self.validator.validate_block(block)
+
+            # Check if the block is valid
+            if not block.is_valid(self.blockchain):
+                # Dev log
+                print('Error occurred! Stopping program (Own block is invalid)')
+
+                # TODO -> Change behaviour when block is invalid
+
+                return False
+
+            # Add block to own temp blocks
+            self.validator.temp_blocks.append(block)
+
+            # Emit block to other nodes
 
 
-        # 8. Cut the socket and rpc connections
+            # Collect all temporary blocks, until time is over (32 seconds)
+            while datetime.now().timestamp() <= self.blockchain.last_blocks[0].timestamp.timestamp() + 32:
+                # Collect blocks and transactions for the next round
+                time_sleep(.05)
 
+            # Select the winner block of the temporary blocks
+            winner_block = self.validator.select_winner(self.blockchain, True)
 
-        # 9. Turn all processes off
+            # Share winner with other nodes, fetch their winner
+            # TODO -> Wait 5 seconds until all possible winners arrived at the input_queue, then select the one with most of the "votes"
+
+            # Add block to the blockchain
+            success = self.blockchain.add_block(winner_block)
+
+            if not success:
+                # Dev log
+                print('Error occurred! Stopping program (add_block was not successful)')
+
+                return False
+
+            print('Added block successfully:', winner_block.index)
+
+            # Every 10 blocks check if whole chain is valid
+            if winner_block.index % 10 == 0:
+                if not self.blockchain.is_valid:
+                    # TODO -> Find the error and resync the chain with other nodes
+
+                    # Dev log
+                    print('Error occurred! Stopping program (Chain is invalid)')
+
+                    return False
+
+                else:
+                    # Dev log
+                    print('Chain is valid')
+
+        # 8. Turn all processes off
 
 
     def start(self):
@@ -96,7 +162,17 @@ class Processor():
         self.database = Database()
 
         # Initialize the blockchain
-        self.blockchain = Blockchain()
+        if not self.genesis_validation:
+            self.blockchain = Blockchain()
+
+        else:
+            # Test values
+            test_tx = Transaction('00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000',
+                        '690ec29bae1791c134acfa0a5f49ebcc43491493e41f751ed319a67db8f75d6dc2acc288b7e7d160ed3362c9490b40ff399047e639a9a0862f6dcd227fbd9f99',
+                        4_096, tx_type='stake')
+            test_tx.signature = '00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000'
+
+            self.blockchain = Blockchain(genesis_tx=[test_tx])
 
         # Start the rpc server
         self.rpc_server = RPCServer(self.blockchain, start=True, db_q=self.database.db_q)
@@ -123,6 +199,10 @@ class Processor():
 
         # Stop the thread
         self.stop_event.set()
+
+        # Cut the socket and rpc connections
+        self.rpc_server.stop()
+        self.rpc_server = None
 
         return True
 
